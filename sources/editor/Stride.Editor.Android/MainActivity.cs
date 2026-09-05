5a65a3d0e2e10055b1cdf5784dc8ea108f4d4dec
gasukinani <gasukinani80@gmail.com>
2026-09-06 05:47:38 +0800

    Update MainActivity.cs

--- sources/editor/Stride.Editor.Android/MainActivity.cs
+++ sources/editor/Stride.Editor.Android/MainActivity.cs
@@ -10,14 +10,13 @@ namespace Stride.Editor.Android;
     Label = "Stride Engine Android",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.KeyboardHidden,
-    ScreenOrientation = ScreenOrientation.Landscape)] // Mas maganda ang landscape para sa laro at editor
-public class MainActivity : AndroidStrideActivity
+    ScreenOrientation = ScreenOrientation.Landscape)]
+public class MainActivity : StrideActivity
 {
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
 
-        // Sinisimulan nito ang mismong Stride Engine Game Context
         Game = new Game();
         Run(Game);
     }
