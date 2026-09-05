994438e84da486b279b819868574b14e3f877f02
gasukinani <gasukinani80@gmail.com>
2026-09-06 05:57:16 +0800

    Update MainActivity.cs

--- sources/editor/Stride.Editor.Android/MainActivity.cs
+++ sources/editor/Stride.Editor.Android/MainActivity.cs
@@ -7,17 +7,26 @@ using Stride.Starter;
 namespace Stride.Editor.Android;
 
 [Activity(
-    Label = "Stride Engine Android",
+    Label = "Stride Editor",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.KeyboardHidden,
     ScreenOrientation = ScreenOrientation.Landscape)]
 public class MainActivity : StrideActivity
 {
+    private Game? _game;
+
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
 
-        Game = new Game();
-        Run(Game);
+        // Tamang syntax: lowercase variable at game.Run()
+        _game = new Game();
+        _game.Run();
+    }
+
+    protected override void OnDestroy()
+    {
+        _game?.Dispose();
+        base.OnDestroy();
     }
 }
