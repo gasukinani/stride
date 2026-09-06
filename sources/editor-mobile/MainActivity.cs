732b1641cb11cf916862be0f3d6f674b7d91b519
gasukinani <gasukinani80@gmail.com>
2026-09-06 19:01:20 +0800

    Update MainActivity.cs

--- sources/editor-mobile/MainActivity.cs
+++ sources/editor-mobile/MainActivity.cs
@@ -2,6 +2,7 @@ using Android.App;
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 using Stride.Engine;
 using Stride.Starter;
 
@@ -27,8 +28,49 @@ namespace StrideStudio.Mobile
         {
             base.OnCreate(savedInstanceState);
 
+            // Tiyaking Fullscreen at nakatago ang System Navigation Bar para sa Game Engine UI
+            HideSystemUI();
+
+            // Simulan ang Stride Game Context
             _game = new EditorGame();
             _game.Run(GameContext);
+        }
+
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            base.OnWindowFocusChanged(hasFocus);
+            if (hasFocus)
+            {
+                HideSystemUI();
+            }
+        }
+
+        private void HideSystemUI()
+        {
+            if (Window == null) return;
+
+            // Immersive Sticky Fullscreen mode para sa Android tablets at phones
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+            {
+                Window.SetDecorFitsSystemWindows(false);
+                var controller = Window.InsetsController;
+                if (controller != null)
+                {
+                    controller.Hide(WindowInsets.Type.StatusBars() | WindowInsets.Type.NavigationBars());
+                    controller.SystemBarsBehavior = (int)WindowInsetsControllerBehavior.ShowTransientBarsBySwipe;
+                }
+            }
+            else
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                var uiOptions = (int)Window.DecorView.SystemUiVisibility;
+                uiOptions |= (int)SystemUiFlags.LowProfile;
+                uiOptions |= (int)SystemUiFlags.Fullscreen;
+                uiOptions |= (int)SystemUiFlags.HideNavigation;
+                uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+                Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+#pragma warning restore CS0618
+            }
         }
 
         protected override void OnDestroy()
